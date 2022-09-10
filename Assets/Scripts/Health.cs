27dfe88b705cfe9ebb6da66308402ca8b7a51435
LAPTOP-25KFC7M6\teloc <telocim@yahoo.com>
2022-09-09 23:06:06 -0700

    score keeper

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,58 +5,68 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] bool isPlayer;
     [SerializeField] int health = 50;
+    [SerializeField] int score = 50;
     [SerializeField] ParticleSystem hitEffect;
+
     [SerializeField] bool applyCameraShake;
     CameraShake cameraShake;
 
     AudioPlayer audioPlayer;
+    ScoreKeeper scoreKeeper;
 
     private void Awake()
     {
         cameraShake = Camera.main.GetComponent<CameraShake>();
         audioPlayer = FindObjectOfType<AudioPlayer>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
         if (damageDealer != null)
         {
-            //take damage 
             TakeDamage(damageDealer.GetDamage());
             PlayHitEffect();
             audioPlayer.PlayDamageClip();
             ShakeCamera();
-
-            //deal damage
             damageDealer.Hit();
-
         }
     }
-
-    private void ShakeCamera()
+    public int GetHealth()
     {
-        if (cameraShake!=null && applyCameraShake)
-        {
-            cameraShake.Play();
-        }
+        return health;
     }
-
     void TakeDamage(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
-            Destroy(gameObject);
+            Die();
         }
     }
-
+    private void Die()
+    {
+        if (!isPlayer)
+        {
+            scoreKeeper.ModifyScore(score);
+        }
+        Destroy(gameObject);
+    }
     void PlayHitEffect()
     {
         if (hitEffect != null)
         {
             ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
             Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
+    }
+    private void ShakeCamera()
+    {
+        if (cameraShake != null && applyCameraShake)
+        {
+            cameraShake.Play();
         }
     }
 }
